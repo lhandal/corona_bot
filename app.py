cca76661a723a43db1135fb2f32967484e67c394
Leandro <lepehan@gmail.com>
2020-05-27 18:10:14 -0400

    deployment test 1

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+import requests
+import json
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+msg.body('this is the response text')
+msg.media('https://example.com/path/image.jpg')
+
+@app.route('/bot', methods=['POST'])
+def bot():
+    incoming_msg = request.values.get('Body', '').lower()
+    print(incoming_msg)
+    resp = MessagingResponse()
+    msg = resp.message()
+    responded = False
+
+    if 'Hi' in incoming_msg :
+        text = f'Hola bola!'
+        msg.body(text)
+        responded = True
+
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5000, debug=True)
